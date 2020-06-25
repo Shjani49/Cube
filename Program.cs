4f4de3e52f7e284ecc7c1509b3ac1daf12eca61a
Shjani49 <shjani49@gmail.com>
2020-06-24 18:59:53 -0600

    Add pow(), convert string to integer

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace cube
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Please Enter a Integer Number:");
+            // Convert String to Integer number.
+            //@ Linc:https://stackoverflow.com/questions/1019793/how-can-i-convert-string-to-int
+            int i = Int32.Parse(Console.ReadLine());
+            // Show the user we colleted the name.
+            Console.WriteLine("You have entered:"+ i);
+            //Display the cube of the number which user enter
+            // Here we convert our output in to integer
+            //pow() function takes two numbers arguments and returns their powers.  
+            Console.WriteLine("Cube :"+ (int)Math.Pow(i,3));
         }
     }
 }
