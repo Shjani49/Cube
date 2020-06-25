4a4e0b9d4e1a7a0616c49ac5dd6b72308f0b2108
Shjani49 <shjani49@gmail.com>
2020-06-24 21:19:50 -0600

    Except 4 digit integer number from user  and calculate the sum of its digits

--- Program.cs
+++ Program.cs
@@ -6,17 +6,47 @@ namespace cube
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            Console.WriteLine("Please Enter a Integer Number:");
-            // Convert String to Integer number.
+            
+            Console.WriteLine("Please Enter a Integer Number..");
+            // Int32.Parse(String) Method is used to convert the string representation of a number to its 32-bit signed integer equivalent.
             //@ Linc:https://stackoverflow.com/questions/1019793/how-can-i-convert-string-to-int
             int i = Int32.Parse(Console.ReadLine());
-            // Show the user we colleted the name.
+            // Show the user we collected the number.
             Console.WriteLine("You have entered:"+ i);
             //Display the cube of the number which user enter
             // Here we convert our output in to integer
             //pow() function takes two numbers arguments and returns their powers.  
             Console.WriteLine("Cube :"+ (int)Math.Pow(i,3));
+
+            Console.WriteLine("Please enter a 4 Digits Number..");
+            // Console. ReadLine reads input from the console... It means we are converting user input value(which is a string) in to a integer...
+            //Int32.Parse(String) Method is used to convert the string representation of a number to its 32-bit signed integer equivalent.
+            int number = Int32.Parse(Console.ReadLine());
+    
+            // Show the user we collected the number.
+            Console.WriteLine("Your Number is:"+ number);
+            //  getSum() is a function that returns the sum of all the items that have been added to the dataset
+            Console.WriteLine("Sum of the Digits is:"+ getSum(number));
+            
+            
         }
+
+        
+        // Calaulate the Sum and return value to the main function
+        //  getSum() is a function that returns the sum of all the items that have been added to the dataset
+        static int getSum(int number)
+        {
+            // Declare a Integer variable that stores our sum..
+            int sum ;
+            // For loop has 3 parts: initialization, condition, and change
+            // Here we Initialize variable sum to zero..
+            //increment number to sum by % 10
+            //number divided by 10 
+            for (sum = 0; number > 0; sum += number % 10,number/= 10); 
+            
+            // return sum 
+            return sum; 
+        }
+        
     }
 }
